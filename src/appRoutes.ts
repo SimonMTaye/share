f03ed3692c0305f7d1cc53f4b42d75b25ebf0a72
SirPotato <mulat.simon@gmail.com>
2018-11-18 11:24:38 +0300

    Set default home route to File Explorer untill  Feature is ready

--- src/appRoutes.ts
+++ src/appRoutes.ts
@@ -4,8 +4,10 @@ import { FileExplorerComponent } from './FileExplorer/file-explorer/file-explore
 import { HostPageComponent } from './NetworkingModule/host-page/host-page.component';
 
 const appRoutes: Routes = [
-    {path: '', component: FrontPageComponent},
+    // {path: '', component: FrontPageComponent},
     {path: 'explore', component: FileExplorerComponent},
+    // Temporaryly set default to File Explorer
+    {path: '', component: FileExplorerComponent},
     {path: 'host', component: HostPageComponent},
     {path: 'landing', component: HostPageComponent}
   ]
